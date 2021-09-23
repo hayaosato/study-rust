b599c3bffa1589db04020881f2e36f5f2e73cafa
hayaosato <hayao.munesato@gmail.com>
2021-09-23 21:09:53 +0900

    ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+    println!("{}", s);
+    let hoge = hoge(&s[..]);
+    println!("{}", hoge);
+}
+
+fn hoge(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
 }
